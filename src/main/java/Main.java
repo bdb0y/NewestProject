26d51d0ee9a1e1180b97f3e62348796dfdc1713b
amin <bandar3eda@yahoo.com>
2020-05-26 16:06:25 +0430

    initial

---
+++ src/main/java/Main.java
@@ -0,0 +1,33 @@
+import java.io.File;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.FileChannel;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.charset.Charset;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+public class Main extends HttpServlet {
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+      throws ServletException, IOException {
+    URL website = new URL("https://cdn.aminrezaei.workers.dev/sx5c_tdd1.mp4");
+    ReadableByteChannel rbc = Channels.newChannel(website.openStream());
+    FileChannel fos = FileChannel.open(Paths.get("myNewFile11.mp4"),
+            StandardOpenOption.CREATE,
+            StandardOpenOption.TRUNCATE_EXISTING,
+            StandardOpenOption.WRITE);
+    long currentTime = System.currentTimeMillis();
+    fos.transferFrom(rbc, 0, Long.MAX_VALUE);
+    long finishedTime = System.currentTimeMillis();
+    File file = new File("myNewFile11.mp4");
+    long fileSize = file.length() / 1000;
+    System.out.println("The file size is: " + fileSize);
+    System.out.println(fileSize / ((finishedTime - currentTime) / 1000));
+    resp.getWriter().println("The file size is: " + fileSize + Charset.defaultCharset());
+    resp.getWriter().println(fileSize / ((finishedTime - currentTime) / 1000));
+  }
+}
